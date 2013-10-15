1390cc50c7554ebb2dc729a4285aaca50d2dd4f1
zhang-lei <zhanglei4517@gmail.com>
2013-10-15 11:00:02 +0800

    modify

--- src/main/java/upload/action/UploadAction.java
+++ src/main/java/upload/action/UploadAction.java
@@ -96,9 +96,9 @@ public class UploadAction extends HttpServlet {
         }
          
         if(this.chunk == this.chunks - 1){
-            System.out.println("用户："+this.user);
-            System.out.println("文件名称："+this.name);
-            System.out.println("上传时间："+this.time);
+            System.out.println("user："+this.user);
+            System.out.println("filename："+this.name);
+            System.out.println("uploadtime："+this.time);
         }
  
         resp.setContentType(JSON);
